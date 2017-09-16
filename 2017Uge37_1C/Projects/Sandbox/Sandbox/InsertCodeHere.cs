a5918ab3b6fa64a82bdcf0b66d26a5db93b0cc59
Per Storgaard Laursen <perl@easj.dk>
2017-09-16 09:30:52 +0200

    Files for week 37 2017, class 1C

---
+++ 2017Uge37_1C/Projects/Sandbox/Sandbox/InsertCodeHere.cs
@@ -0,0 +1,29 @@
+﻿using System;
+#pragma warning disable 168
+
+namespace Sandbox
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            Console.WriteLine("Goodbye cruel world!");
+
+            int age = 49;         
+
+            double height = 1.86;
+
+            string name = "Per Laursen";
+
+            bool isMarried = false;
+
+
+
+
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
