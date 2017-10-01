3802ddc5b1db264bdd352180625316b2c0bab14d
Per Storgaard Laursen <perl@easj.dk>
2017-10-01 13:31:54 +0200

    Added ClassroomB
    Added ClassroomB

---
+++ 2017Uge39_1C/Projects/ClassroomB/ClassroomB/Warrior.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ClassroomB
+{
+    public class Warrior
+    {
+        #region Instance fields
+        private string _name;
+        private int _hitPoints;
+
+        private static Random _generator = new Random();
+        #endregion
+
+        #region Constructor
+        public Warrior(string name, int hitPoints)
+        {
+            _name = name;
+            _hitPoints = hitPoints;
+        }
+        #endregion
+
+        #region Properties
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public int HitPoints
+        {
+            get { return _hitPoints; }
+        }
+
+        public bool Dead
+        {
+            get { return _hitPoints <= 0; }
+        }
+        #endregion
+
+        #region Methods
+        public void ReceiveDamage(int damagePoints)
+        {
+            _hitPoints = _hitPoints - damagePoints;
+        }
+
+        public int DealDamage()
+        {
+            return _generator.Next(21) + 10;
+        }
+        #endregion
+    }
+}
